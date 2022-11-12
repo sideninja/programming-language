031db5557a53d0749903328bac09fbada50e3224
Gregor G <gregor@gololicic.si>
2022-11-12 23:47:24 +0100

    add let statement

--- ast/ast.go
+++ ast/ast.go
@@ -1,5 +1,10 @@
 package ast
 
+import (
+	"bytes"
+	"language/tokens"
+)
+
 type Node interface {
 	TokenLiteral() string
 }
@@ -12,4 +17,40 @@ type Statement interface {
 type Expression interface {
 	Node
 	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	var buf bytes.Buffer
+	for _, s := range p.Statements {
+		buf.WriteString(s.TokenLiteral())
+	}
+
+	return buf.String()
+}
+
+type LetStatement struct {
+	Token      tokens.Token
+	Identifier Identifier
+	Right      Expression
+}
+
+func (a *LetStatement) statementNode() {}
+
+func (a *LetStatement) TokenLiteral() string {
+	return a.Token.Literal
+}
+
+type Identifier struct {
+	Token tokens.Token
+	Value string
+}
+
+func (i *Identifier) expressionNode() {}
+
+func (i *Identifier) TokenLiteral() string {
+	return i.Token.Literal
 }
